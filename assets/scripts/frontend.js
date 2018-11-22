cff062e82595039fb19c163851544bad869ead31
pilar <sauleturme@gmail.com>
2018-11-22 17:50:42 +0100

    created a new home and the animation

--- assets/scripts/frontend.js
+++ assets/scripts/frontend.js
@@ -1,23 +1,26 @@
-import $ from "jquery";
-import Popper from "popper.js";
+// import $ from "jquery";
+// import Popper from "popper.js";
 import bootstrap from "bootstrap";
+import Typed from "typed.js";
 
 console.log("js file loaded");
 
-$(function() {
-  if (typeof Storage != "undefined") {
-    if (!sessionStorage.getItem("done")) {
-      setTimeout(function() {
-        $("#contactFormModal").modal("show");
-        sessionStorage.setItem("done", true);
-      }, 5000);
-    }
-  }
-});
-//
-// import Typed from "typed.js";
-//
-// let typedCursor = new Typed('#typed-cursor', {
-//   strings: ["Learn digital skills with us to get the most fulfilling jobs."],
-//   typeSpeed: 2000
+// $(function() {
+//   if (typeof Storage != "undefined") {
+//     if (!sessionStorage.getItem("done")) {
+//       setTimeout(function() {
+//         $("#contactFormModal").modal("show");
+//         sessionStorage.setItem("done", true);
+//       }, 5000);
+//     }
+//   }
 // });
+//
+
+
+let typedCursor = new Typed('.subtitle', {
+  strings: ["Learn digital skills with us to get the most fulfilling jobs."],
+  typeSpeed: 30,
+  loop: true
+});
+console.log(typedCursor)
