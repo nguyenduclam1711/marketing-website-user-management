d7918544d388ef04d9e200bb9c1fbc0b0fa027bb
pilar <sauleturme@gmail.com>
2018-11-27 10:38:14 +0100

    fixed styles of the  home section

--- public/frontend.bundle.js
+++ public/frontend.bundle.js
@@ -114,13 +114,32 @@ console.log("js file loaded"); // $(function() {
 //   }
 // });
 //
-
-var typedCursor = new typed_js__WEBPACK_IMPORTED_MODULE_1___default.a('.subtitle', {
-  strings: ["Learn digital skills with us to get the most fulfilling jobs."],
-  typeSpeed: 30,
-  loop: true
-});
-console.log(typedCursor);
+// 
+// let typedCursor = new Typed('.subtitle', {
+//   strings: ["Learn digital skills with us to get the most fulfilling jobs."],
+//   typeSpeed: 30,
+//   loop: true
+// });
+// console.log(typedCursor)
+//nav-sticky
+// const nav = document.querySelector('#headernav');
+// const navTop = nav.offsetTop;
+//
+// function stickyNavigation() {
+//   console.log('navTop = ' + navTop);
+//   console.log('scrollY = ' + window.scrollY);
+//
+//   if (window.scrollY >= navTop) {
+//     // nav offsetHeight = height of nav
+//     document.body.style.paddingTop = nav.offsetHeight + 'px';
+//     document.body.classList.add('fixed-nav');
+//   } else {
+//     document.body.style.paddingTop = 0;
+//     document.body.classList.remove('fixed-nav');
+//   }
+// }
+//
+// window.addEventListener('scroll', stickyNavigation);
 
 /***/ }),
 
