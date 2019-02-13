a15b3721d8d75a8d070261a7999f68be2389c74e
Shteyn <tarna83@mail.ru>
2019-02-13 11:36:36 +0100

    sort events by location

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -4,25 +4,19 @@ const Event = require('../models/event');
 const Location = require('../models/location');
 
 module.exports.getEvents = async (req, res) => {
-  let events = await Event.find().populate("location").sort({ 'start': -1 }).limit(8);
-
-  locationEvents = events.reduce(function (acc, obj) {
-    //console.log('from Events Controller', acc)
-    if (obj.location) {
-      if (!acc[obj.location.name]) {
-        acc[obj.location.name] = [];
-      }
-      acc[obj.location.name].push(obj);
-      return acc;
-    } else {
-      return acc
-    }
-  }, {});
-
+  const locations = await Location.find({});
+  let eventsByLocation = [];
+  for await (let loc of locations) {
+    eventsByLocation.push({
+      name: loc.name,
+      events: await Event.find({ location: loc._id }).populate('location').sort({ 'start': -1 }).limit(3)
+    })
+  }
   res.render('events', {
-    locationEvents
+    eventsByLocation
   })
 }
+
 module.exports.getEventsByLocation = async (req, res) => {
   let location = await Location.findOne({ "name": { $regex: new RegExp(req.params.location, "i") } })
 
