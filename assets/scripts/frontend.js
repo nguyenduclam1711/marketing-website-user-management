8529e512486fb668316d8aeba971c3c13e359a68
spielhoelle <spiel.hoelle@live.de>
2019-04-16 16:29:40 +0200

    nl templating

--- assets/scripts/frontend.js
+++ assets/scripts/frontend.js
@@ -1,79 +1,85 @@
 // import $ from "jquery";
 // import Popper from "popper.js";
-import 'bootstrap/js/dist/util';
-import 'bootstrap/js/dist/modal';
-import 'bootstrap/js/dist/scrollspy';
-import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/collapse';
-import 'bootstrap/js/dist/alert';
-
+import "bootstrap/js/dist/util";
+import "bootstrap/js/dist/modal";
+import "bootstrap/js/dist/scrollspy";
+import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/collapse";
+import "bootstrap/js/dist/alert";
 
 require("../css/style.scss");
+const toggleNL = (remove = false) => {
+  document.getElementById("nlbtn").disabled = remove ? "" : "disabled";
+  document.getElementById("nlbtn").innerHTML = remove ? "Subscribe" : "Loading";
+  document
+    .getElementById("mc-embedded-subscribe-form")
+    .classList.toggle("half-transparent");
+};
 
-(function () {
+(function() {
   var newsletterForm = document.querySelector("#mc-embedded-subscribe-form");
   if (newsletterForm) {
-    newsletterForm.addEventListener("submit", function (e) {
+    newsletterForm.addEventListener("submit", function(e) {
+      e.preventDefault();
       var email = document.querySelector("#mce-EMAIL").value.trim();
       if (email) {
-        e.preventDefault();
+        toggleNL();
         fetch("/newsletter-signup", {
-          method: 'POST',
+          method: "POST",
           headers: {
             "content-type": "application/json"
           },
           body: JSON.stringify({ email }) // data can be `string` or {object}!
-
-      }).then(res => res.json())
-        .then(response => {
-          if (response.code === 200) {
-            var img = `<img src='/media/spinner.svg' class="mx-auto d-block"/>`
-            var html = `
-            <div class="alert alert-success" role="alert">
-              Thanks for subscribing. Please check your Mail.
-            </div>
-            `
-            document.getElementById("mc-embedded-subscribe-form").innerHTML = img
-            setTimeout(() => {
-              document.getElementById("mc-embedded-subscribe-form").parentElement.innerHTML = html          
-            }, 3000)
-          } else {
-            let img = `<img src='/media/spinner.svg' class="mx-auto d-block"/>`
-            let html = `<div class="alert alert-danger" role="alert">
-                An error occured. Please try again later!
-              </div>`
-            document.getElementById("mc-embedded-subscribe-form").innerHTML = img
-            setTimeout(() => {
-              document.getElementById("mc-embedded-subscribe-form").parentElement.innerHTML = html
-            }, 3000)
-          }
         })
-        .catch(error => {
-          console.error('Error:', error)
-        });
+          .then(res => res.json())
+          .then(response => {
+            const nlHeadline = document.getElementById("nlheadline");
+
+            if (response.code === 200) {
+              nlHeadline.innerHTML = response.message;
+            } else if (response.code === 422) {
+              toggleNL(true);
+              nlHeadline.innerHTML =
+                "User alread in list, check your mails for a existing verification mail";
+            }
+          })
+          .catch(error => {
+            console.error("Error:", error);
+          });
       }
-    })
+    });
   }
 })();
 //
 
-$('.counter-count').each(function () {
-  $(this).prop('Counter', 0).animate({
-    Counter: $(this).text()
-  }, {
-      duration: 5000,
-      easing: 'swing',
-      step: function (now) {
-        $(this).text(Math.ceil(now));
+$(".counter-count").each(function() {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text()
+      },
+      {
+        duration: 5000,
+        easing: "swing",
+        step: function(now) {
+          $(this).text(Math.ceil(now));
+        }
       }
-    });
+    );
 });
-var scrollbuttons = document.getElementsByClassName('scrollbutton');
+var scrollbuttons = document.getElementsByClassName("scrollbutton");
 for (var i = 0, len = scrollbuttons.length; i < len; i++) {
-  scrollbuttons[i].addEventListener('click', function (event) {
-    event.preventDefault()
-    document.querySelector(event.target.attributes.href.value).scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'})
-  })
+  scrollbuttons[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    document
+      .querySelector(event.target.attributes.href.value)
+      .scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+      });
+  });
 }
 
 //
