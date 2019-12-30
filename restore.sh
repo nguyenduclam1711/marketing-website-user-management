7f8501f1e5ba0c81b8a057d75a04b22f8ed1b9fc
spielhoelle <spiel.hoelle@live.de>
2019-12-30 18:30:49 +0100

    chore: ignore webstorm files and restore script

--- restore.sh
+++
@@ -1,3 +0,0 @@
-cd ./backup
-dirname=$(ls -dt ./* | head -1)
-scp -rp /Users/thomaskuhnert/develop/dci/marketing-website/backup/$dirname/uploads/* dci@46.101.220.207:/home/dci/marketing-website/uploads
