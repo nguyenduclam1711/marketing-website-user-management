1429d0c5a4262de64af7d1ec2fb15aca5fbaa6a3
Tommy <spielhoelle@posteo.net>
2020-02-09 04:51:28 +0100

    fix: render react just on root existence

--- assets/scripts/settings.js
+++ assets/scripts/settings.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import StringTranslation from './StringTranslation.jsx'
-ReactDOM.render(
-  <div>
-    {/*This is rendered with  React-->*/}
-    <StringTranslation/>
-  </div>,
-  document.getElementById('stringtranslationroot')
-);
+
+let settingsRoot = document.getElementById('stringtranslationroot');
+if (settingsRoot) {
+  ReactDOM.render(
+    <div>
+      {/*This is rendered with  React-->*/}
+      <StringTranslation/>
+    </div>,
+    settingsRoot
+  );
+}
