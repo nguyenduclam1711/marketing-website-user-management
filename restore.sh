04424011d0bcf78c4e84c0c13ef21230b1963c91
Tommy <spielhoelle@posteo.net>
2020-03-24 15:43:25 +0900

    feat: optional staging shared_image restore destination

--- restore.sh
+++ restore.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 dirname=$(ls -dt ./backup/20* | head -1) && scp -rp $dirname/uploads/ dci@46.101.220.207:/home/dci/digitalcareerinstitute.org/
+#restore staging env
+#dirname=$(ls -dt ./backup/20* | head -1) && scp -rp $dirname/uploads/ dci@46.101.220.207:/home/dci/staging.digitalcareerinstitute.org/
