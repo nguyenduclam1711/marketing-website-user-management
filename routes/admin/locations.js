7e954ccdb2b27e5fb39b35ad8db184acf1b31bdd
Tommy <spielhoelle@posteo.net>
2020-05-02 14:01:02 +0900

    fix: add missing routes from contact-branch

--- routes/admin/locations.js
+++ routes/admin/locations.js
@@ -3,15 +3,38 @@ const router = express.Router();
 const AdminLocationsController = require('../../controllers/admin/AdminLocationsController')
 const { ensureAuthenticated, redirectNonAdmin } = require("../../helpers/helper");
 
+router.get("/",
+    ensureAuthenticated,
+    redirectNonAdmin,
+    AdminLocationsController.getLocations);
 
-router.get("/", ensureAuthenticated, redirectNonAdmin, AdminLocationsController.getLocations);
+router.get("/:id",
+    ensureAuthenticated,
+    redirectNonAdmin,
+    AdminLocationsController.getSingleLocation);
 
-router.get("/:id", ensureAuthenticated, redirectNonAdmin, AdminLocationsController.getSingleLocation);
+router.get("/edit/:id",
+    ensureAuthenticated,
+    redirectNonAdmin,
+    AdminLocationsController.editLocation);
 
-router.get("/edit/:id", ensureAuthenticated, redirectNonAdmin, AdminLocationsController.editLocation);
+router.post("/",
+    ensureAuthenticated,
+    redirectNonAdmin,
+    AdminLocationsController.uploadImages,
+    AdminLocationsController.resizeImages,
+    AdminLocationsController.createLocation);
 
-router.post("/", ensureAuthenticated, redirectNonAdmin, AdminLocationsController.createLocation);
+router.delete("/delete/:id",
+    ensureAuthenticated,
+    redirectNonAdmin,
+    AdminLocationsController.deleteLocation);
 
-router.delete("/delete/:id", ensureAuthenticated, redirectNonAdmin, AdminLocationsController.deleteLocation);
-router.put("/update/:id", ensureAuthenticated, redirectNonAdmin, AdminLocationsController.updateLocation);
+router.put("/update/:id",
+    ensureAuthenticated,
+    redirectNonAdmin,
+    AdminLocationsController.uploadImages,
+    AdminLocationsController.resizeImages,
+    AdminLocationsController.updateLocation);
+
 module.exports = router;
