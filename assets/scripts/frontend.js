dc05a4ad8f895cdd5d1761052e1cc46bbddeebd6
Tommy <spielhoelle@posteo.net>
2020-05-15 09:51:15 +0900

    feat: activate tracking after acc. or rej.

--- assets/scripts/frontend.js
+++ assets/scripts/frontend.js
@@ -311,16 +311,21 @@ if (notFoundTimer) {
     }
   }, 1000);
 }
+const findCookie = (name) => {
+  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  if (match) return match[2];
+}
 
 const googleAnalyticsdomainname = window.location.hostname;
-var googleAnalyticsRejectCookie = "dcigoogleAnalyticsoptout" + googleAnalyticsdomainname.replace(/\./g, "");
-var googleAnalyticsShowncookiename = "dciprivacy" + googleAnalyticsdomainname.replace(/\./g, "");
+const googleAnalyticsShowncookiename = "dciprivacy" + googleAnalyticsdomainname.replace(/\./g, "");
 
 //actual GA tracking
+if (document.cookie.indexOf(googleAnalyticsShowncookiename) !== -1) {
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-document.cookie.indexOf(googleAnalyticsRejectCookie) !== -1 ? gtag('config', 'UA-140962410-1', { 'anonymize_ip': true }) : gtag('config', 'UA-140962410-1');
+  findCookie(googleAnalyticsShowncookiename) === "optedout" ? gtag('config', 'UA-140962410-1', { 'anonymize_ip': true }) : gtag('config', 'UA-140962410-1');
+}
 //actual GA tracking end
 
 const cookieButton = document.getElementById("cookiehint");
@@ -328,24 +333,22 @@ const cookieRejectButton = document.getElementById("cookieRejectButton");
 if (cookieRejectButton) {
   cookieRejectButton.addEventListener("click", e => {
     e.preventDefault();
-    var googleAnalyticsRejectCookievalue = googleAnalyticsRejectCookie + "=" + "optedout;expires=Wed, 1 Jan 2100 00:00:00 UTC;path=/";
-    document.cookie = googleAnalyticsRejectCookievalue;
-    hideCookieHint();
+    hideCookieHint("optedout");
   });
 }
 if (cookieButton) {
-  if (document.cookie.indexOf(googleAnalyticsShowncookiename) < 0) {
+  if (!findCookie(googleAnalyticsShowncookiename)) {
     setTimeout(function () {
       cookieButton.classList.add("shown");
     }, 500);
   }
   document.getElementById("cookieCloseButton").addEventListener("click", e => {
       e.preventDefault();
-      hideCookieHint();
+      hideCookieHint("accepted");
     });
 }
-const hideCookieHint = () => {
-  var googleAnalyticsShowncookienamevalue = googleAnalyticsShowncookiename + "=" + "shown;expires=Wed, 1 Jan 2100 00:00:00 UTC;path=/";
+const hideCookieHint = (cookieValue) => {
+  const googleAnalyticsShowncookienamevalue = googleAnalyticsShowncookiename + "=" + `${cookieValue};expires=Wed, 1 Jan 2100 00:00:00 UTC;path=/`;
   document.cookie = googleAnalyticsShowncookienamevalue;
   cookieButton.classList.remove("shown");
 };
