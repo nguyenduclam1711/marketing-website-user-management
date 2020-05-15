fe1f8b0ec79385e79dbca46940779ba5f1406dda
Tommy <spielhoelle@posteo.net>
2020-05-15 10:53:04 +0900

    chore: outsource ga code to sep. file

--- assets/scripts/frontend.js
+++ assets/scripts/frontend.js
@@ -7,6 +7,7 @@ import "bootstrap/js/dist/carousel";
 import "bootstrap/js/dist/alert";
 import {alertTimeout} from "./helper.js"
 require("./polygons");
+require("./google-analytics");
 require("../css/style.scss");
 
 const toggleNL = (remove = false) => {
@@ -311,44 +312,3 @@ if (notFoundTimer) {
     }
   }, 1000);
 }
-const findCookie = (name) => {
-  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-  if (match) return match[2];
-}
-
-const googleAnalyticsdomainname = window.location.hostname;
-const googleAnalyticsShowncookiename = "dciprivacy" + googleAnalyticsdomainname.replace(/\./g, "");
-
-//actual GA tracking
-if (document.cookie.indexOf(googleAnalyticsShowncookiename) !== -1) {
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-  findCookie(googleAnalyticsShowncookiename) === "optedout" ? gtag('config', 'UA-140962410-1', { 'anonymize_ip': true }) : gtag('config', 'UA-140962410-1');
-}
-//actual GA tracking end
-
-const cookieButton = document.getElementById("cookiehint");
-const cookieRejectButton = document.getElementById("cookieRejectButton");
-if (cookieRejectButton) {
-  cookieRejectButton.addEventListener("click", e => {
-    e.preventDefault();
-    hideCookieHint("optedout");
-  });
-}
-if (cookieButton) {
-  if (!findCookie(googleAnalyticsShowncookiename)) {
-    setTimeout(function () {
-      cookieButton.classList.add("shown");
-    }, 500);
-  }
-  document.getElementById("cookieCloseButton").addEventListener("click", e => {
-      e.preventDefault();
-      hideCookieHint("accepted");
-    });
-}
-const hideCookieHint = (cookieValue) => {
-  const googleAnalyticsShowncookienamevalue = googleAnalyticsShowncookiename + "=" + `${cookieValue};expires=Wed, 1 Jan 2100 00:00:00 UTC;path=/`;
-  document.cookie = googleAnalyticsShowncookienamevalue;
-  cookieButton.classList.remove("shown");
-};
