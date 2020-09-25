ede7eed7021980bbe8ec8eed352b60a51cb38624
Tommy <spielhoelle@posteo.net>
2020-09-26 00:20:24 +0900

    fix: change csv delimiter

--- assets/scripts/frontend.js
+++ assets/scripts/frontend.js
@@ -192,7 +192,7 @@ getFileElements.forEach(el => {
 function objectToCsv(data) {
   const csvRows = [];
   const headers = Object.keys(data[0]);
-  csvRows.push(headers.join(","));
+  csvRows.push(headers.join(";"));
   for (const row of data) {
     const values = headers.map(header => {
       const escaped = !!row[header]
@@ -200,7 +200,7 @@ function objectToCsv(data) {
         : "";
       return `"${escaped}"`;
     });
-    csvRows.push(values.join(","));
+    csvRows.push(values.join(";"));
   }
   return csvRows.join("\n");
 }
