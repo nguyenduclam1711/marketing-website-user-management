a9d26043ba34e27ce9b27a58d10e754068499695
Tommy <spielhoelle@posteo.net>
2021-06-16 14:50:37 -0500

    feat: opitimize canvas, ansers accorion, wire questions

--- assets/scripts/QuestionsDiagram.jsx
+++ assets/scripts/QuestionsDiagram.jsx
@@ -32,8 +32,10 @@ engine.setModel(model);
 
 
 const CanvasWrapper = styled.div`
+  flex-grow: 1;
+  overflow: hidden;
   & > div {
-    height: 100vh;
+    height: 100%;
     width: 100vw;
   }
 `
@@ -62,12 +64,13 @@ function QuestionsDiagram(props) {
 
   const addQuestion = (e) => {
     e.preventDefault()
+    console.log('e.target.elements', e.target.elements);
     const node = new DefaultNodeModel({
-      name: !!e.target.elements.freequestion.checked ? "Freequestion" : e.target.elements.input.value,
-      color: !!e.target.elements.freequestion.checked ? "rgb(0, 128, 229)" : e.target.elements.input.dataset.color,
+      name: !!e.target.elements.freequestion && !!e.target.elements.freequestion.checked ? "Freequestion" : e.target.elements.input.value,
+      color: !!e.target.elements.freequestion && !!e.target.elements.freequestion.checked ? "rgb(0, 128, 229)" : e.target.elements.input.dataset.color,
       customType: e.target.elements.input.dataset.type,
       extras: {
-        freequestion: !!e.target.elements.freequestion.checked
+        freequestion: !!e.target.elements.freequestion && !!e.target.elements.freequestion.checked
       }
     });
     node.setPosition(600, 100);
@@ -81,58 +84,58 @@ function QuestionsDiagram(props) {
 
   console.log(answeravailable);
   return (
-    <div>
-      <button className="btn btn-secondary badge ml-2" type='button' data-container='body' data-toggle='popover' data-trigger="hover" data-placement='top' data-content='Link questions to one or multiple answers. If a question is followed by a freeanswer, it should be the only anwer of that question'> ? </button>
-      <form onSubmit={addQuestion}>
-        <div className="form-group">
-          <label htmlFor="addquestion">Add Question</label>
-          <input className="form-control" name="input" type="text" data-type="question" data-color="rgb(0, 128, 129)" style={{ borderColor: "rgb(0, 128, 129)", borderStyle: "solid" }} id="addquestion" />
-        </div>
-      </form>
-      <label htmlFor="addanswer">Add Answer</label>
-      <form className="form-inline" onSubmit={addQuestion}>
-        <div className="form-group w-100">
-          <input className="form-control w-100" name="input" type="text" data-type="answer" data-color="rgb(255, 204, 1)" style={{ borderColor: "rgb(255, 204, 1)", borderStyle: "solid" }} id="addanswer" disabled={!answeravailable} />
-          <button className="btn btn-primary" type="submit">Add</button>
-        </div>
-        <div className="form-group w-100">
-          <div className="form-group form-check  mb-3">
-            <input type="checkbox" name="freequestion" className="form-check-input" style={{ borderColor: "rgb(255, 204, 1)", borderStyle: "solid" }} id="freequestion" onChange={() => setansweravailable(!answeravailable)} />
-            <label className="form-check-label" htmlFor="freequestion">Free answer</label>
-            <input type="checkbox" name="entityanswer" className="form-check-input" style={{ borderColor: "rgb(255, 204, 1)", borderStyle: "solid" }} id="entityanswer" onChange={() => setansweravailable(!answeravailable ? "entityanswer" : answeravailable)} />
-            <label className="form-check-label" htmlFor="entityanswer">Entityanswer</label>
+    <div className="h-100 d-flex flex-column">
+      <div>
+        <form onSubmit={addQuestion}>
+          <div className="form-group">
+            <label htmlFor="addquestion">Add Question</label>
+            <input className="form-control" name="input" type="text" data-type="question" data-color="rgb(0, 128, 129)" style={{ borderColor: "rgb(0, 128, 129)", borderStyle: "solid" }} id="addquestion" />
           </div>
-        </div>
-      </form>
-      <button className="btn btn-primary btn-sm mr-2"
-        disabled={loading}
-        onClick={() => {
-          setloading(true)
-          let itemMap = {};
-          model.getSelectedEntities().map(item => {
-            let newItem = item.clone(itemMap)
-            model.addNode(newItem)
-            newItem.setPosition(newItem.getX() + 20, newItem.getY() + 20)
-            engine.setModel(model);
-            item.setSelected(!1)
-            setloading(false)
-          })
-        }}>{loading ? "Loading" : "Clone selected"}</button>
-      <button className="btn btn-success btn-sm"
-        disabled={loading}
-        onClick={() => {
-          setloading(true)
-          fetch(`/admin/questions/update`, {
-            method: "POST",
-            headers: {
-              "content-type": "application/json"
-            },
-            body: JSON.stringify(model.serialize())
-          }).then(res => res.json())
-            .then(res => {
+          <button className="btn btn-primary" type="submit">Add</button>
+        </form>
+        <label htmlFor="addanswer">Add Answer</label>
+        <form className="form-inline" onSubmit={addQuestion}>
+          <div className="form-group w-100">
+            <input className="form-control w-100" name="input" type="text" data-type="answer" data-color="rgb(255, 204, 1)" style={{ borderColor: "rgb(255, 204, 1)", borderStyle: "solid" }} id="addanswer" disabled={!answeravailable} />
+            <button className="btn btn-primary" type="submit">Add</button>
+          </div>
+          <div className="form-group w-100">
+            <div className="form-group form-check  mb-3">
+              <input type="checkbox" name="freequestion" className="form-check-input" style={{ borderColor: "rgb(255, 204, 1)", borderStyle: "solid" }} id="freequestion" onChange={() => setansweravailable(!answeravailable)} />
+              <label className="form-check-label" htmlFor="freequestion">Free answer</label>
+            </div>
+          </div>
+        </form>
+        <button className="btn btn-primary btn-sm mr-2"
+          disabled={loading}
+          onClick={() => {
+            setloading(true)
+            let itemMap = {};
+            model.getSelectedEntities().map(item => {
+              let newItem = item.clone(itemMap)
+              model.addNode(newItem)
+              newItem.setPosition(newItem.getX() + 20, newItem.getY() + 20)
+              engine.setModel(model);
+              item.setSelected(!1)
               setloading(false)
             })
-        }}>{loading ? "Loading" : "Save"}</button>
+          }}>{loading ? "Loading" : "Clone selected"}</button>
+        <button className="btn btn-success btn-sm"
+          disabled={loading}
+          onClick={() => {
+            setloading(true)
+            fetch(`/admin/questions/update`, {
+              method: "POST",
+              headers: {
+                "content-type": "application/json"
+              },
+              body: JSON.stringify(model.serialize())
+            }).then(res => res.json())
+              .then(res => {
+                setloading(false)
+              })
+          }}>{loading ? "Loading" : "Save"}</button>
+      </div>
       <CanvasWrapper>
         <CanvasWidget id='canvas' engine={engine} />
       </CanvasWrapper>
--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -5,61 +5,10 @@ import QuestionsDiagram from './QuestionsDiagram.jsx'
 let settingsRoot = document.getElementById('questionsdiagramroot');
 if (settingsRoot) {
   ReactDOM.render(
-    <div>
+    <div className="h-100">
       {/*This is rendered with  React-->*/}
-      <QuestionsDiagram/>
+      <QuestionsDiagram />
     </div>,
     settingsRoot
   );
 }
-
-
-
-
-
-// {
-//     "Are you looking for a carreer change?": {
-//         "yes": {
-//             "Are you older than 45?": {
-//                 "no": {
-//                     "What is your professional situation?": {
-//                         "Full / Partime job": { },
-//                         "Student / Trainee": { },
-//                         "Unemployed": {
-//                             "Are you registered with the Bundesagentur or Agentur fuer Arbeit?": {
-//                                 "Jobcenter": {},
-//                                 "Agentur fuer Arbeit": {},
-//                                 "No": {}
-//                             }
-//                          }
-//                     }
-//                 },
-//                 "yes": {
-//                     "What is your professional situation?": {
-//                         "Full / Partime job": { },
-//                         "Student / Trainee": { },
-//                         "Unemployed": { }
-//                     }
-//                 }
-//             }
-//         },
-//         "no": {
-//             "Are you older than 45?": {
-//                 "no": {
-//                     "What is your professional situation?": {
-//                         "Full / Partime job": { },
-//                         "Student / Trainee": { },
-//                         "Unemployed": { }
-//                     }
-//                 },
-//                 "yes": {
-//                     "What is your professional situation?": {
-//                         "Full / Partime job": { },
-//                         "Student / Trainee": { },
-//                         "Unemployed": { }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
