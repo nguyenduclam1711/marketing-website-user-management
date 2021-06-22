4daff57cb4c9b43f0b97c248ab393fd304fbf9f3
Thomas Kuhnert <spielhoelle@posteo.net>
2021-06-21 22:01:56 -0500

    fix: render neverbounce also on static html

--- assets/scripts/frontend.js
+++ assets/scripts/frontend.js
@@ -588,3 +588,11 @@ const jumpToNextQuestion = (e, diagramNodes, model) => {
       })
   }
 }
+
+const input = document.querySelector('input[name*="phone"]')
+if (input) {
+  const iti = intlTelInput(input, {
+    initialCountry: "de",
+    utilsScript: utilsScript
+  });
+}
