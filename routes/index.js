d15a1d03845cd1f47cfdd6d28f321e98e923285b
Thomas Kuhnert <spielhoelle@posteo.net>
2021-07-28 16:42:07 -0500

    chore: remove unused route

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,6 @@ router.get("/contact", IndexController.contactLocations);
 
 router.post("/contact", IndexController.contact);
 
-router.post("/downloadcurriculum", IndexController.downloadCourseCurriculum);
-
 router.get("/tour", IndexController.tour);
 
 router.post("/newsletter-signup", IndexController.newsletter);
