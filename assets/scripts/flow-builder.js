f50aeeb8d8c1ee44ba84b2fb0e80d0127f456ef7
Thomas Kuhnert <spielhoelle@posteo.net>
2021-12-08 15:39:56 +0700

    Revert "Revert "Merge pull request #447 from DigitalCareerInstitute/test-hide-question-by-query-string3""
    This reverts commit 7ed021d8b5c1af5ff71fc5aee5a859fa54758f78.

--- assets/scripts/flow-builder.js
+++ assets/scripts/flow-builder.js
@@ -97,12 +97,12 @@ const jumpToNextQuestion = (e, diagramNodes, flow) => {
 		submitButton.innerText = "Loading..."
 		submitButton.disabled = true
 		let payload = JSON.parse(localStorage.getItem(`dcianswers_${flow.name}`))
-		let hiddenQuestionWhichShouldStillBeenProcessed = Array.from(searchParams.entries()).reduce((acc, i) => {
-			if (searchParams.getAll(i[0]).length > 1) {
-				acc.push(i)
+		let hiddenQuestionWhichShouldStillBeenProcessed = Array.from(searchParams.entries()).map(i => [i[0].replace('push_', ""), i[1]]).reduce((acc, i) => {
+			if (Array.from(searchParams.entries()).map(i => [i[0].replace('push_', ""), i[1]]).filter(a => a[0] === i[0]).length) {
+				acc.push([i[0].replace('push_', ''), i[1]])
 			}
 			return acc
-		}, []).filter(i => i[1] !== "false")
+		}, []).filter(i => i[1] !== "false" && i[0] !== "dci_blank")
 		if (hiddenQuestionWhichShouldStillBeenProcessed.length > 0) {
 			hiddenQuestionWhichShouldStillBeenProcessed.forEach(q => {
 				payload[q[0]] = q[1]
